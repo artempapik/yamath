7eafb7e53c480094ea85de2a1f436a7c748ae4e8
artempapik <artemhbk13@gmail.com>
2022-01-12 14:50:24 +0200

    program themes to get from list

--- app.js
+++ app.js
@@ -18,6 +18,72 @@ const articleToTitle = {
   'integers': 'Натуральные числа'
 }
 
+const themes = {
+  algebra: {
+    integers: [
+      {
+        name: 'ряд натуральных чисел',
+        href: 'integers'
+      },
+      { name: 'разряды натуральных чисел' },
+      { name: 'сравнение натуральных чисел' },
+      {},
+      { name: 'сложение натуральных чисел' },
+      { name: 'вычитание натуральных чисел' },
+      { name: 'умножение. свойства умножения' },
+      { name: 'деление. деление с остатком' },
+      { name: 'степень числа' },
+      {},
+      { name: 'числовые и буквенные выражения. формулы' },
+      { name: 'уравнения' },
+      { name: 'комбинаторные задачи' }
+    ],
+    fractionals: [
+      { name: ' обычные дроби' },
+      { name: ' правильные и неправильные дроби. сравнение дробей' },
+      { name: ' сложение и вычитание дробей с одинаковыми знаменателями' },
+      { name: ' дроби и деление натуральных чисел' },
+      { name: ' мешаные числа' },
+      {},
+      { name: ' десятичные дроби' },
+      { name: ' сравнение десятичных дробей' },
+      { name: ' округление чисел' },
+      { name: ' сложение и вычитание десятичных дробей' },
+      { name: ' умножение десятичных дробей' },
+      { name: ' деление десятичных дробей' },
+      {},
+      { name: ' среднее арифметическое' },
+      { name: ' проценты' }
+    ]
+  },
+  geometry: [
+    { name: 'отрезок' },
+    { name: 'плоскость. прямая. луч' },
+    { name: 'шкала. координатный луч' },
+    {},
+    { name: 'угол. виды углов. измерение углов' },
+    { name: 'многоугольники. равные фигуры' },
+    { name: 'треугольник' },
+    { name: 'прямоугольник' },
+    {},
+    { name: 'площадь. площадь прямоугольника' },
+    { name: 'прямоугольный параллелепипед. пирамида' },
+    { name: 'объём прямоугольного параллелепипеда' }
+  ]
+}
+
+for (let i = 0; i < themes.algebra.integers.length; i++) {
+  themes.algebra.integers[i].href = `articles/${themes.algebra.integers[i].href}`
+}
+
+for (let i = 0; i < themes.algebra.fractionals.length; i++) {
+  themes.algebra.fractionals[i].href = `articles/${themes.algebra.fractionals[i].href}`
+}
+
+for (let i = 0; i < themes.geometry.length; i++) {
+  themes.geometry[i].href = `articles/${themes.geometry[i].href}`
+}
+
 app.get('/articles/:article', (req, res) => {
   const article = `${req.params.article}.pug`
   const path = `${dirname}/views/${article}`
@@ -30,7 +96,7 @@ app.get('/articles/:article', (req, res) => {
   res.render(article, { title: 'Ряд натуральных чисел' /* articleToTitle[article] think about */ })
 })
 
-app.get('/', (_, res) => res.render('startpage.pug'))
+app.get('/', (_, res) => res.render('startpage.pug', { themes }))
 
 app.get('/error', (_, res) => res.render('error.pug', { title: articleToTitle['error'] }))
 
