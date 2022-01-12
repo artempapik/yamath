d70aea7b47aca5e41e6905e219e75f8ed04d5e9b
artempapik <artemhbk13@gmail.com>
2022-01-12 14:00:07 +0200

    write main page; create 'views' folder; refactroring

--- app.js
+++ app.js
@@ -14,26 +14,24 @@ app.use(favicon(`${dirname}/ico/math-logo.ico`))
 
 const articleToTitle = {
   'error': 'Ошибка',
+  // rewrite
   'integers': 'Натуральные числа'
 }
 
 app.get('/articles/:article', (req, res) => {
-  const article = req.params.article
-  const path = `${dirname}/${article}.pug`
+  const article = `${req.params.article}.pug`
+  const path = `${dirname}/views/${article}`
 
   if (!fs.existsSync(path)) {
     res.redirect('/error')
     return
   }
 
-  res.render(path, { title: articleToTitle[article] })
+  res.render(article, { title: 'Ряд натуральных чисел' /* articleToTitle[article] think about */ })
 })
 
-app.get('/', (_, res) => {
-  // temporary
-  res.redirect('/articles/integers')
-})
+app.get('/', (_, res) => res.render('startpage.pug'))
 
-app.get('/error', (_, res) => res.render(`${dirname}/error.pug`, { title: articleToTitle['error'] }))
+app.get('/error', (_, res) => res.render('error.pug', { title: articleToTitle['error'] }))
 
 app.listen(port)
