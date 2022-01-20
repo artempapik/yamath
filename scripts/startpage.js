9ec1ddfba444e02dcfd78f075bd75a0c1cb838f8
artempapik <artemhbk13@gmail.com>
2022-01-20 21:08:14 +0200

    xaxa

--- scripts/startpage.js
+++ scripts/startpage.js
@@ -219,7 +219,7 @@ assignInput(inputMobile)
 
 if (inputMobile) {
   document.onclick = () => {
-    setTimeout(() => window.scrollTo(0, 0), 50)
+    setTimeout(() => window.scrollTo(0, 0), 10)
     inputMobile.style.inputMode = 'none'
   }
   inputMobile.onpointerup = () => inputMobile.focus()
