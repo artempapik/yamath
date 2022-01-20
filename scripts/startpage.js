7d22455669f4fe327bd632f40f6f196cce911d52
artempapik <artemhbk13@gmail.com>
2022-01-20 21:07:11 +0200

    1

--- scripts/startpage.js
+++ scripts/startpage.js
@@ -219,7 +219,7 @@ assignInput(inputMobile)
 
 if (inputMobile) {
   document.onclick = () => {
-    setTimeout(() => window.scrollTo(0, 0), 200)
+    setTimeout(() => window.scrollTo(0, 0), 50)
     inputMobile.style.inputMode = 'none'
   }
   inputMobile.onpointerup = () => inputMobile.focus()
