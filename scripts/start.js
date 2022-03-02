1ea52c6b7dc93b281de0b4a25c5cae578c4c9928
artempapik <artemhbk13@gmail.com>
2022-03-02 18:37:44 +0200

    454

--- scripts/start.js
+++ scripts/start.js
@@ -13,7 +13,7 @@ let a = getComputedStyle(document.documentElement).getPropertyValue('--text-colo
   element.onpointerup = () => {
     element.style.background = '#202020'
 
-    document.documentElement.style.setProperty('--aa', '#202020')
+    document.documentElement.style.setProperty('--aa', 'red')
 
     // changeIconStyle(iconDiv, '#f4c744', 1.2, 1)
     window.location.pathname = element.classList[0]
