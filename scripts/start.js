0c7bb7a2b7ab881cf57396565bf2c53c403a8ac6
artempapik <artemhbk13@gmail.com>
2022-03-02 15:00:36 +0200

    111

--- scripts/start.js
+++ scripts/start.js
@@ -3,10 +3,10 @@ import { colors } from '../data.js'
 ['.forms', '.themes', '.levels'].map(selector => document.querySelector(selector)).forEach(element => {
   const iconDiv = element.children[0]
   element.onmouseover = () => changeIconStyle(iconDiv, '#f4c744', 1.2, 1)
-  element.ontouchstart = () => changeIconStyle(iconDiv, '#f4c744', 1.2, 1)
+  element.onpointerover = () => changeIconStyle(iconDiv, '#f4c744', 1.2, 1)
 
   element.onmouseout = () => changeIconStyle(iconDiv, getTextColor(), 1, .8)
-  element.onfocuslost = () => changeIconStyle(iconDiv, 'green', 1, .8)
+  // element.onfocuslost = () => changeIconStyle(iconDiv, 'green', 1, .8)
 
   element.onpointerup = () => window.location.pathname = element.classList[0]
 })
