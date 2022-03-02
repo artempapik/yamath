0b7c35f53ed1d49211f8e255713dbfaca0ca66fb
artempapik <artemhbk13@gmail.com>
2022-03-02 15:47:08 +0200

    1

--- scripts/start.js
+++ scripts/start.js
@@ -5,6 +5,7 @@ const isMobile = 'ontouchstart' in window
 ;['.forms', '.themes', '.levels'].map(selector => document.querySelector(selector)).forEach(element => {
   const iconDiv = element.children[0]
   if (!isMobile) element.onmouseover = () => changeIconStyle(iconDiv, '#f4c744', 1.2, 1)
+  if (isMobile) element.onmouseover = () => changeIconStyle(iconDiv, getTextColor(), 1.2, 1)
   element.onmouseout = () => changeIconStyle(iconDiv, getTextColor(), 1, .8)
 
   element.onpointerup = () => {
