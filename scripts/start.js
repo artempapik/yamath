7248429a40b8dbc4936deea2553de34228b57092
artempapik <artemhbk13@gmail.com>
2022-03-05 21:44:08 +0200

    ???

--- scripts/start.js
+++ scripts/start.js
@@ -7,9 +7,8 @@ const changeIconStyle = (iconDiv, color, scale) => {
 
 document.querySelectorAll('li').forEach(category => {
   const iconDiv = category.children[0]
-  changeIconStyle(iconDiv, '', 1)
   category.onmouseover = () => changeIconStyle(iconDiv, '#f4c744', 1.2)
-  category.onmouseout = () => changeIconStyle(iconDiv, '', 1)
+  category.onpointerout = () => changeIconStyle(iconDiv, '', 1)
 })
 
 const input = document.querySelector('input')
