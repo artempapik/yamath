c0cbe0f3d4c3f6a81aabf66aa01ea4f79a5aad57
artempapik <artemhbk13@gmail.com>
2022-03-07 20:21:46 +0200

    rf

--- scripts/start.js
+++ scripts/start.js
@@ -119,6 +119,6 @@ const startEn = [
 const translations = [startRu, startUa, startEn]
 
 document.querySelector('.question').onpointerup = () => {
-  if (isMobile) setTimeout(() => {}, 1)
+  if (isMobile) setTimeout(() => {}, 10)
   document.querySelector('footer').style.display = 'block'
 }
